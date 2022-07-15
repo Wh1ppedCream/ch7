b1600c73b6c1dc67baa555849562c562d1ddecdd
Talha Hussain <talh768@gmail.com>
2022-07-15 12:42:39 -0400

    done ex7.5.6

--- ex7.5.6/debug.cpp
+++ ex7.5.6/debug.cpp
@@ -2,7 +2,11 @@ class Debug {
 	public:
 		constexpr Debug(bool b = true) : hw(b), io(b), other(b) { }
 		constexpr Debug(bool h, i, o) : hw(h), io(i), other(o) { }
+		
+		constexpr bool any() {return hw || io || other; }
 
+		//no constexpr as the values will be set to be constant and it also sets the values at compile time,
+		//while we might also want to change the values while running the code
 		void hw_set(bool h) {
 			hw = h;
 		}
